1ef40370f7c78c183993a88c1795ecd17df60e3b
RoGreen19 <rogreen1903@gmail.com>
2023-04-30 21:40:20 +0100

    Change Warning in app file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div class="container">
-        <div class="card text-bg-dark">
+      <div className="container">
+        <div className="card text-bg-dark">
           <img
             src="scr/20210118-barnimages-2.jpg"
-            class="card-img"
+            className="card-img"
             alt="Wearher"
           />
-          <div class="card-img-overlay">
+          <div className="card-img-overlay">
             <h2 id="city-name">Kyiv</h2>
             <h4 id="current-date">05/12</h4>
             <h5 id="current-time">12:01</h5>
@@ -19,92 +19,91 @@ export default function App() {
                 id="search-new-city"
                 type="search"
                 placeholder="Search city"
-                autocomplete="off"
+                autoComplete="off"
               />
-              <button class="search-button">🔍</button>
+              <button className="search-button">🔍</button>
             </form>
-            <img class="weather-img-current" src="200-232.png" />
+            <img className="weather-img-current" src="200-232.png" />
             <h1 id="day-temperasture">-12°</h1>
             <h6 id="current-forecast">Cloudy</h6>
             <h6 id="humidity">Humidity: 10%</h6>
             <h6 id="wind">Wind: 13 mph</h6>
-            <div class="container text-center" id="forecast">
-              <div class="row">
-                <div class="col">
-                  <p class="weekdate-one-name">Mon</p>
+            <div className="container text-center" id="forecast">
+              <div className="row">
+                <div className="col">
+                  <p className="weekdate-one-name">Mon</p>
                   <br />
                   <img
                     src="http://openweathermap.org/img/wn/10d@2x.png"
-                    class="week-weather-icon"
+                    className="week-weather-icon"
                   />
                   <br />
-                  <p class="weekdate-one-temperature">
-                    <span class="max-temperature">8°</span>
-                    <span class="min-temperature">/-12°</span>
+                  <p className="weekdate-one-temperature">
+                    <span className="max-temperature">8°</span>
+                    <span className="min-temperature">/-12°</span>
                   </p>
                 </div>
-                <div class="col">
-                  <p class="weekdate-two-name">Tues</p>
+                <div className="col">
+                  <p className="weekdate-two-name">Tues</p>
                   <br />
                   <img
                     src="http://openweathermap.org/img/wn/13d@2x.png"
-                    class="week-weather-icon"
+                    className="week-weather-icon"
                   />
                   <br />
-                  <p class="weekdate-two-temperature">
-                    <span class="max-temperature">8°</span>
-                    <span class="min-temperature">/-12°</span>
+                  <p className="weekdate-two-temperature">
+                    <span className="max-temperature">8°</span>
+                    <span className="min-temperature">/-12°</span>
                   </p>
                 </div>
-                <div class="col">
-                  <p class="weekdate-three-name">Wed</p>
+                <div className="col">
+                  <p className="weekdate-three-name">Wed</p>
                   <br />
                   <img
                     src="http://openweathermap.org/img/wn/10d@2x.png"
-                    class="week-weather-icon"
+                    className="week-weather-icon"
                   />
                   <br />
-                  <p class="weekdate-three-temperature">
-                    <span class="max-temperature">8°</span>
-                    <span class="min-temperature">/-12°</span>
+                  <p className="weekdate-three-temperature">
+                    <span className="max-temperature">8°</span>
+                    <span className="min-temperature">/-12°</span>
                   </p>
                 </div>
-                <div class="col">
-                  <p class="weekdate-four-name">Thurs</p>
+                <div className="col">
+                  <p className="weekdate-four-name">Thurs</p>
                   <br />
                   <img
                     src="http://openweathermap.org/img/wn/09d@2x.png"
-                    class="week-weather-icon"
+                    className="week-weather-icon"
                   />
                   <br />
-                  <p class="weekdate-four-temperature">
-                    <span class="max-temperature">8°</span>
-                    <span class="min-temperature">/-12°</span>
+                  <p className="weekdate-four-temperature">
+                    <span className="max-temperature">8°</span>
+                    <span className="min-temperature">/-12°</span>
                   </p>
                 </div>
-                <div class="col">
-                  <p class="weekdate-five-name">Fri</p>
+                <div className="col">
+                  <p className="weekdate-five-name">Fri</p>
                   <br />
                   <img
                     src="http://openweathermap.org/img/wn/50d@2x.png"
-                    class="week-weather-icon"
+                    className="week-weather-icon"
                   />
                   <br />
-                  <p class="weekdate-five-temperature">
-                    <span class="max-temperature">8°</span>
-                    <span class="min-temperature">/-12°</span>
+                  <p className="weekdate-five-temperature">
+                    <span className="max-temperature">8°</span>
+                    <span className="min-temperature">/-12°</span>
                   </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <p class="code-link">
+        <p className="code-link">
           {" "}
           <a
             href="https://github.com/RoGreen19/Weather-App-Final-Project"
             target="_blank"
-            without
             rel="noreferrer"
             style={{ color: "#F9A90F" }}
           >
